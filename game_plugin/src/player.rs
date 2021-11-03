2a752384ad16923ef4f405493c8b431331439884
Nick Alexeev <nickalexeev@protonmail.com>
2021-11-02 17:23:13 -0800

    added terrain

--- game_plugin/src/player.rs
+++ game_plugin/src/player.rs
@@ -1,11 +1,173 @@
 use crate::actions::Actions;
 use crate::loading::TextureAssets;
 use crate::GameState;
-use bevy::prelude::*;
-
+use bevy::{
+    prelude::*,
+    render::{mesh::Indices, pipeline::PrimitiveTopology},
+};
+use nalgebra::Vector3;
 pub struct PlayerPlugin;
 
 pub struct Player;
+struct Grid {
+    points: Vec<f32>,
+    x: usize,
+    y: usize,
+}
+impl Grid {
+    pub fn x(&self) -> usize {
+        self.x
+    }
+    pub fn y(&self) -> usize {
+        self.y
+    }
+    /// gets points unchecked at point
+    pub fn get(&self, x: usize, y: usize) -> f32 {
+        self.points[self.y * x + y]
+    }
+}
+pub struct Water {
+    grid: Grid,
+}
+impl Water {
+    pub fn new() -> Self {
+        Self {
+            grid: Grid {
+                points: vec![0.0; 100 * 100],
+                x: 100,
+                y: 100,
+            },
+        }
+    }
+    /// Builds mesh from grid, todo: make water sim inplace for performance reasons
+    pub fn build_mesh(&self) -> Mesh {
+        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        let mut vertices = vec![];
+        let mut position = vec![];
+        let mut normals = vec![];
+        let mut uvs = vec![];
+        for x in 0..self.grid.x() - 1 {
+            for y in 0..self.grid.y() - 1 {
+                let x0_y0 = Vector3::new(x as f32, self.grid.get(x, y) as f32, y as f32);
+                let x0_y1 = Vector3::new(x as f32, self.grid.get(x, y + 1) as f32, y as f32 + 1.0);
+                let x1_y0 = Vector3::new(x as f32 + 1.0, self.grid.get(x + 1, y), y as f32);
+                let x1_y1 = Vector3::new(
+                    x as f32 + 1.0,
+                    self.grid.get(x + 1, y + 1) as f32,
+                    y as f32 + 1.0,
+                );
+                let triangle0_normal = (x0_y1 - x0_y0).cross(&(x1_y0 - x0_y0)).normalize();
+                let triangle1_normal = (x1_y0 - x1_y1).cross(&(x0_y1 - x1_y1)).normalize();
+
+                //vert 0
+                position.push([x0_y0.x, x0_y0.y, x0_y0.z]);
+                normals.push([triangle0_normal.x, triangle0_normal.y, triangle0_normal.z]);
+                uvs.push([0.0, 0.0]);
+
+                //vert 1
+                position.push([x0_y1.x, x0_y1.y, x0_y1.z]);
+                normals.push([triangle0_normal.x, triangle0_normal.y, triangle0_normal.z]);
+                uvs.push([0.0, 1.0]);
+                //vert 2
+                position.push([x1_y0.x, x1_y0.y, x1_y0.z]);
+                normals.push([triangle0_normal.x, triangle0_normal.y, triangle0_normal.z]);
+                uvs.push([1.0, 0.0]);
+
+                //Triangle 1
+                //vert3
+                position.push([x1_y1.x, x1_y1.y, x1_y1.z]);
+                normals.push([triangle1_normal.x, triangle1_normal.y, triangle1_normal.z]);
+                uvs.push([1.0, 0.0]);
+
+                //vert4
+                position.push([x1_y0.x, x1_y0.y, x1_y0.z]);
+                normals.push([triangle1_normal.x, triangle1_normal.y, triangle1_normal.z]);
+                uvs.push([1.0, 1.0]);
+                //vert5
+                position.push([x0_y1.x, x0_y1.y, x0_y1.z]);
+                normals.push([triangle1_normal.x, triangle1_normal.y, triangle1_normal.z]);
+                uvs.push([0.0, 1.0]);
+
+                vertices.append(&mut vec![
+                    //uv
+                    0.0,
+                    0.0,
+                    //normal
+                    triangle0_normal.x,
+                    triangle0_normal.y,
+                    triangle0_normal.z,
+                    //position:
+                    x0_y1.x,
+                    x0_y1.y,
+                    x0_y1.z,
+                    //uv
+                    0.0,
+                    1.0,
+                    //normal
+                    triangle0_normal.x,
+                    triangle0_normal.y,
+                    triangle0_normal.z,
+                    //position:
+                    x1_y0.x,
+                    x1_y0.y,
+                    x1_y0.z,
+                    //uv
+                    1.0,
+                    0.0,
+                    //normal
+                    triangle0_normal.x,
+                    triangle0_normal.y,
+                    triangle0_normal.z,
+                    //triangle 1
+
+                    //position:
+                    x0_y1.x,
+                    x0_y1.y,
+                    x0_y1.z,
+                    //uv
+                    0.0,
+                    1.0,
+                    //normal
+                    triangle1_normal.x,
+                    triangle1_normal.y,
+                    triangle1_normal.z,
+                    //position:
+                    x1_y1.x,
+                    x1_y1.y,
+                    x1_y1.z,
+                    //uv
+                    1.0,
+                    1.0,
+                    //normal
+                    triangle1_normal.x,
+                    triangle1_normal.y,
+                    triangle1_normal.z,
+                    //position:
+                    x1_y0.x,
+                    x1_y0.y,
+                    x1_y0.z,
+                    //uv
+                    1.0,
+                    0.0,
+                    //normal
+                    triangle1_normal.x,
+                    triangle1_normal.y,
+                    triangle1_normal.z,
+                ]);
+            }
+        }
+        println!("pos length: {}", position.len());
+        println!("normal length: {}", normals.len());
+        println!("uv length: {}", uvs.len());
+        let indicies = (0..position.len()).map(|i| i as u32).collect();
+        mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, position);
+        mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+        mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+        mesh.set_indices(Some(Indices::U32(indicies)));
+        return mesh;
+        todo!()
+    }
+}
 
 /// This plugin handles player related stuff like movement
 /// Player logic is only active during the State `GameState::Playing`
@@ -21,21 +183,34 @@ impl Plugin for PlayerPlugin {
 }
 
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
 }
 
 fn spawn_player(
     mut commands: Commands,
     textures: Res<TextureAssets>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    let water = Water::new();
+    let mut transform = Transform::from_translation(Vec3::new(0.3, 0.5, 0.3));
+    transform.scale = Vec3::new(0.1, 0.1, 0.1);
     commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.add(textures.texture_bevy.clone().into()),
-            transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
+        .spawn_bundle(PbrBundle {
+            material: materials.add(Color::rgb(0.0, 0.5, 0.0).into()),
+            transform: transform,
+            mesh: meshes.add(water.build_mesh()),
             ..Default::default()
         })
+        .insert(water)
         .insert(Player);
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
 }
 
 fn move_player(
@@ -46,7 +221,7 @@ fn move_player(
     if actions.player_movement.is_none() {
         return;
     }
-    let speed = 150.;
+    let speed = 1.0;
     let movement = Vec3::new(
         actions.player_movement.unwrap().x * speed * time.delta_seconds(),
         actions.player_movement.unwrap().y * speed * time.delta_seconds(),
